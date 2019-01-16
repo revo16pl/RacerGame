274eff9ca574c4c208de4b2c753e1931285a3a38
Revo <radoslawrak@outlook.com>
2019-01-16 22:23:44 +0100

    added coin prefab with script;

---
+++ Assets/Scripts/coin.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class coin : MonoBehaviour
+{
+    public bool isActive = true;
+
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        transform.rotation = Quaternion.Euler(0, Time.timeSinceLevelLoad * 60f, -90);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.tag == "Player")
+        {
+            GetComponent<MeshRenderer>().enabled = false;
+            GetComponent<AudioSource>().Play();
+            isActive = false;
+        }
+    }
+}
