10d3372e5aef0fe922bcc957bb746cd124729f1c
Revo <radoslawrak@outlook.com>
2019-01-17 19:33:23 +0100

    Modified racetrack; added speeder script;

---
+++ Assets/Scripts/speeder.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class speeder : MonoBehaviour
+{
+    public GameObject player;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            var rigidbody = player.GetComponent<Rigidbody>();
+            var movement = FindObjectOfType<movement>();
+
+            rigidbody.AddForce((Vector3.right) * 2000f);
+        }
+    }
+}
